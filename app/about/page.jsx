1cac29937002351617ac9ba7d13340add7201413
Kerry Kreiter <wonproudamerican@yahoo.com>
2024-10-05 12:38:18 -0500

    added how to become a Christian

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,43 @@
 export default function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[600px] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>About Page</h1>
+      <p>HOW THE SPIRITUAL VITAMINS DEVELOPED</p>
+      <p>
+        While reading my Devotions one morning about 2 years ago I heard a voice
+        (yes, it was audible for me) say, “Get my Word out!” I understood it to
+        be God speaking to me. It was not a dream as I was wide awake. I
+        answered and said, “Lord, I am a nobody. I am not a preacher nor do I
+        teach women or children in church. How can I get Your Word out?”
+      </p>{" "}
+      <p>
+        Looking back on the event that was a pretty stupid thing to say. Did I
+        think God didn’t know who He was talking to? He repeated, “Get my Word
+        out”. Later I told my husband, Tim, about the encounter and we discussed
+        possibilities. After prayer we decided to produce a form of what we call
+        “Spiritual Vitamins”.
+      </p>
+      <p>
+        The original vitamins were made of construction paper shaped like a
+        capsule. They contained a Title, Date and Scripture verse, all hand
+        lettered, and colored . I mailed 20 copies to my adult Kreiter family
+        and it took me 30 days to create 20 vitamins. They were not fancy at
+        all.
+      </p>
+      <p>
+        Months later Tim offered the idea that he could print the vitamins on
+        cardstock on the computer, in color and even find computer art work for
+        the front. I added an additional Quote on the back side. This continued
+        until recently when our son, Kerry, offered to design a Kreiter Family
+        web page to include all past, present and future vitamins.
+      </p>
+      <p>
+        Now the amazing thing is that Kerry does not work with computers or the
+        design of web pages. But after several months of research, and teaching
+        himself, Kerry has created “Stage three” of the Spiritual Vitamins
+        development. On February 10th we are launching the Spiritual Vitamin web
+        page and if you are inclined to use it we hope that it is a blessing to
+        you.
+      </p>
     </div>
   );
 }
--- app/christianity/page.jsx
+++ app/christianity/page.jsx
@@ -2,7 +2,30 @@ export default function Christianity() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[600px] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="h-40"></div>
-      <h1>How to receive Christ and become a Christian</h1>
+      <p>Would you like to ask Jesus to be your personal Lord and Savior?</p>
+
+      <p>You can pray this prayer right now:</p>
+
+      <p>
+        “Dear God, I know I’m a sinner, and I ask for your forgiveness. I
+        believe Jesus Christ is Your Son. I believe that He died for my sin and
+        that you raised Him to life. I want to trust Him as my Savior and follow
+        Him as Lord, from this day forward. Guide my life and help me to do your
+        will. I pray this in the name of Jesus. Amen.”
+      </p>
+      <p>
+        Did you pray this prayer? The Bible says if you confess your sins and
+        believe in you heart that Jesus Christ died for your sins and rose
+        again, then you are a Christian!
+      </p>
+      <p>
+        Begin your new journey by reading the Bible to discover exactly how much
+        God loves you and His plan for your life!
+      </p>
+      <p>
+        You are a child of God and belong to the family of believers called
+        Christians! Welcome!!
+      </p>
     </div>
   );
 }
