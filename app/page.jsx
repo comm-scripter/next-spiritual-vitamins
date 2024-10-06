02177dc2962801e362866223d41a65493dda34ed
Kerry Kreiter <wonproudamerican@yahoo.com>
2024-10-05 20:39:24 -0500

    update home styles

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,9 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[4px_1fr_20px] items-center justify-center min-h-[600px] w-full p-4 pb-8 gap-10 sm:p-8 font-[family-name:var(--font-geist-sans)] bg-gradient-to-r from-blue-200 to-emerald-100">
+    <div className="grid grid-rows-[4px_1fr_20px] items-center justify-center h-screen w-full p-4 pb-8 gap-10 sm:p-8 font-[family-name:var(--font-geist-sans)] bg-gradient-to-r from-blue-200 to-emerald-100">
       <div className="flex"></div>
       <Hero></Hero>
-      <h1>Next Spiritual Vitamins</h1>
     </div>
   );
 }
